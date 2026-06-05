63942a3edb4ba0c037fe8961fc75e363b81ae291
riricardo <ricardo.augusto.cardona@email.com>
2026-06-05 12:18:58 +0100

    Actions config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+const repositoryName = 'japanese-learning-games';
+
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: './',
-});
+  base: command === 'build' ? `/${repositoryName}/` : '/',
+}));
